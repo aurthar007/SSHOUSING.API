631a27ee3d67bdb6fee39db85bd9b28fcc31c25b
Avijit Gorai <avijitgorai8250@gmail.com>
2025-05-19 09:26:27 +0530

    Update all

--- SSHOUSING.Domain/Entities/User.cs
+++ SSHOUSING.Domain/Entities/User.cs
@@ -9,10 +9,6 @@ public class User
     [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "First name is required.")]
-    public string Firstname { get; set; }
-    public string Lastname { get; set; }
-
     [Required(ErrorMessage = "Password is required.")]
     public string Password { get; set; }
 
@@ -20,5 +16,5 @@ public class User
     [MaxLength(10, ErrorMessage = "Mobile number can't exceed 20 characters.")]
     [Phone(ErrorMessage = "Invalid Mobile number format.")]
     public string Mobile { get; set; }
-}
+} 
 
