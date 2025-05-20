d247e9ec07874417c5b76e940e71055d50dfc7e8
Avijit Gorai <avijitgorai8250@gmail.com>
2025-05-20 10:39:42 +0530

    change program css

--- SSHOUSING.API/Program.cs
+++ SSHOUSING.API/Program.cs
@@ -1,54 +1,29 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using SSHOUSING.Domain.Entities;
 using SSHOUSING.Domain.Interface;
 using SSHOUSING.Infrastucture;
 using SSHOUSING.Infrastucture.Repository;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add DbContext
+// Add services to the container.
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Register repositories
 builder.Services.AddScoped<ICountry, CountryRepository>();
 builder.Services.AddScoped<IState, StateRepository>();
 builder.Services.AddScoped<IDistrict, DistrictRepository>();
+builder.Services.AddScoped<IUser, UserRepository>();
 
-// Configure JWT Authentication
-var jwtSettings = builder.Configuration.GetSection("Jwt");
-var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = true;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings["Issuer"],
-        ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key)
-    };
-});
-
-// Add controllers and Swagger
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -57,8 +32,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Enable authentication & authorization middleware
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
