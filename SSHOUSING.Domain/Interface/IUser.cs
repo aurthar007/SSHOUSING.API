cc56e12f52527016138d1d7421dd12f4d077dfde
Avijit Gorai <avijitgorai8250@gmail.com>
2025-05-20 10:48:32 +0530

    add i user

--- SSHOUSING.Domain/Interface/IUser.cs
+++ SSHOUSING.Domain/Interface/IUser.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace SSHOUSING.Domain.Interface
 {
-    internal class IUser
+    public interface IUser
     {
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        bool Create(User user);
+        bool Update(User user);
+        bool Delete(int id);
     }
 }
