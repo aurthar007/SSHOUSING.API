28d3f1f0b5520055dccdbe8140b7e609ef87f3b6
Avijit Gorai <avijitgorai8250@gmail.com>
2025-05-20 17:45:06 +0530

    Register workiing

--- SSHOUSING.Infrastucture/Repository/UserRepository.cs
+++ SSHOUSING.Infrastucture/Repository/UserRepository.cs
@@ -6,20 +6,12 @@ namespace SSHOUSING.Infrastucture.Repository
 {
     public class UserRepository : IUser
     {
+
+
         private readonly ApplicationDbContext _context;
         public UserRepository(ApplicationDbContext context)
         {
             _context = context;
-        }
-
-        public bool Add(State state)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Add(User user)
-        {
-            throw new NotImplementedException();
         }
 
         public bool AddUser(User user)
@@ -28,45 +20,6 @@ namespace SSHOUSING.Infrastucture.Repository
             _context.SaveChanges();
             return true;
         }
-
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<State> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public State GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public User Login(string email, string password)
-        {
-            return _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
-        }
-
-        public bool Update(State state)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Update(User user)
-        {
-            throw new NotImplementedException();
-        }
-
-        List<User> IUser.GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        User IUser.GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
+    
