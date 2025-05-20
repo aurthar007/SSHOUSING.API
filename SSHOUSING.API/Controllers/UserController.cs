b7bc5131aab3d6afc05aa80c7a6d4b0349e3f5f9
Avijit Gorai <avijitgorai8250@gmail.com>
2025-05-20 11:01:43 +0530

    Resolve the controller issue

--- SSHOUSING.API/Controllers/UserController.cs
+++ SSHOUSING.API/Controllers/UserController.cs
@@ -1,55 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using SSHOUSING.Domain.Entities;
-using SSHOUSING.Domain.Interface;
 
 namespace SSHOUSING.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        private readonly IUser _user;
-
-        public UserController(IUser user)
+    
+        [Route("api/[controller]")]
+        [ApiController]
+        public class UserController : ControllerBase
         {
-            _user = user;
-        }
+            private readonly IUser _user;
 
-        [HttpGet("GetAllUsers")]
-        public IActionResult GetAllUsers()
-        {
-            var result = _user.GetAll();
-            return Ok(result);
-        }
+            public UserController(IUser user)
+            {
+                _user = user;
+            }
 
-        [HttpGet("GetUserById/{id}")]
-        public IActionResult GetUserById(int id)
-        {
-            var result = _user.GetById(id);
-            if (result == null)
-                return NotFound();
-            return Ok(result);
-        }
+            [HttpGet("GetAllUsers")]
+            public IActionResult GetAllUsers()
+            {
+                return Ok(_user.GetAll());
+            }
 
-        [HttpPost("AddUser")]
-        public IActionResult AddUser(User user)
-        {
-            var result = _user.Create(user);
-            return Ok(result);
-        }
+            [HttpGet("GetUserById/{id}")]
+            public IActionResult GetUserById(int id)
+            {
+                var result = _user.GetById(id);
+                if (result == null) return NotFound();
+                return Ok(result);
+            }
 
-        [HttpPut("UpdateUser")]
-        public IActionResult UpdateUser(User user)
-        {
-            var result = _user.Update(user);
-            return Ok(result);
-        }
+            [HttpPost("AddUser")]
+            public IActionResult AddUser(User user)
+            {
+                return Ok(_user.Create(user));
+            }
 
-        [HttpDelete("DeleteUser/{id}")]
-        public IActionResult DeleteUser(int id)
-        {
-            var result = _user.Delete(id);
-            return Ok(result);
+            [HttpPut("UpdateUser")]
+            public IActionResult UpdateUser(User user)
+            {
+                return Ok(_user.Update(user));
+            }
+
+            [HttpDelete("DeleteUser/{id}")]
+            public IActionResult DeleteUser(int id)
+            {
+                return Ok(_user.Delete(id));
+            }
         }
     }
-}
+
