d9a277012ddfeaa367fac35c14e918a8129f8b7a
Avijit Gorai <avijitgorai8250@gmail.com>
2025-05-20 10:34:38 +0530

    change user file

---
+++ SSHOUSING.Domain/Entities/IUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSHOUSING.Domain.Entities
+{
+    public interface IUser
+    {
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        bool Create(User user);
+        bool Update(User user);
+        bool Delete(int id);
+    }
+}
